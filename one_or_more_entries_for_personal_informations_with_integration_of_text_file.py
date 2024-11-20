21d5c6a8be10ef4eeb8bf69af6ee50d62a83a287
Alexander James E. Lunas <lunasalexander41@gmail.com>
2024-11-20 23:58:32 +0800

    Now I able to create the foundation for the input system of my program; without the loop and proper handling/evaluation of each personal information. I might modify these algorithm along the way if I able to think for more good idea that I can add/implement.

--- one_or_more_entries_for_personal_informations_with_integration_of_text_file.py
+++ one_or_more_entries_for_personal_informations_with_integration_of_text_file.py
@@ -1,2 +1,12 @@
+
+
+user_name = input("Name: ")
+user_age = input("Age: ")
+user_address = input("Full Address: ")
+user_gender = input("Gender (Male/Female): ")
+
 with open("user(s)_informations.txt", "a") as database:
-    database.write("Good Evening Everyone")
+    database.write(user_name + "\n")
+    database.write(user_age + "\n")
+    database.write(user_address + "\n")
+    database.write(user_gender + "\n")
