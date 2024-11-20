6f08e52ab328dbd6031f9f49c69f4725babf5f2b
Alexander James E. Lunas <lunasalexander41@gmail.com>
2024-11-21 01:59:23 +0800

    I implemented now the evaluation for each personal informations; these were an already existing functions in my previous projects so I basically just copied it or duplicate it and implemented it in here with of course essential and preferenced modifications. As you notice it only collects 4 details and not in a minimum of 5, obviously I will work on that, I just want to commit/highlight my progress here in this program, to inform y'all that I'm progressively building the foundation or main algorithm of these program. This is gonna be much faster now since I know the basics now for these program (its intended purpose/objective).

--- one_or_more_entries_for_personal_informations_with_integration_of_text_file.py
+++ one_or_more_entries_for_personal_informations_with_integration_of_text_file.py
@@ -1,9 +1,50 @@
+allowed_special_characters = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/"
+
+while True:
+    while True:
+        user_name = input("Name: ").strip()
+        replacing_spaced_names = user_name.replace(" ", "")
+        if all(char.isalpha() or char in allowed_special_characters for char in replacing_spaced_names):
+            break 
+        elif replacing_spaced_names.isdigit():
+            print("Invalid input, can't have numbers.")
+        else:
+            print("The inputted character(s) is/are invalid.")
+            print("Please try again.")
+
+    while True:
+        user_age = input("Age: ").strip()
+        if user_age.isdigit():
+            integered_user_age = int(user_age)
+            if integered_user_age <= 120:
+                break  
+            else:
+                print("Invalid input, age must be less than or equal to 120.")
+        else:
+            print("Invalid input, must be a digit.")
+            print("Please try again.")
+
+    while True:
+        user_address = input("Full Address: ").strip()
+        replacing_spaced_names_2 = user_address.replace(" ", "")
+        if all(char.isalnum() or char in allowed_special_characters for char in replacing_spaced_names_2):
+            break 
+        else:
+            print("The inputted character(s) is/are invalid.")
+            print("Please try again.")
+
+    while True:
+        user_gender = input("Gender (Male/Female): ").strip().capitalize()
+        if user_gender == "Male" or user_gender == "Female":
+            break 
+        else:
+            print("Invalid input, 'Male' or 'Female' only.")
+            print("Please try again.")
 
 
-user_name = input("Name: ")
-user_age = input("Age: ")
-user_address = input("Full Address: ")
-user_gender = input("Gender (Male/Female): ")
+
+
+
 
 with open("user(s)_informations.txt", "a") as database:
     database.write(user_name + "\n")
